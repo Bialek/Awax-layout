9e375cfea97eed05668549c57e3a603ba9c984ee
Rafał Białek <bialekrafal94@gmail.com>
2018-06-09 16:15:27 +0200

    add share section start contact section add map

--- js/map.js
+++ js/map.js
@@ -1,8 +1,37 @@
-function myMap() {
-    var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-    var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    var marker = new google.maps.Marker({position:myCenter});
-    marker.setMap(map);
+function map() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: -37.817279,lng: 144.952048}, 
+        zoom:17, 
+        scrollwheel:false, 
+        draggable:true,
+        disableDefaultUI: true,
+    });
+
+    var style = [
+        {
+          featureType: "all",
+          elementType: "all",
+          stylers: [
+            { saturation: -100 } 
+          ]
+        }
+    ];
+
+    var mapType = new google.maps.StyledMapType(style, { name:"Grayscale" });    
+    map.mapTypes.set('tehgrayz', mapType);
+    map.setMapTypeId('tehgrayz');
+
+    var marker = {
+        url: "../images/marker.png", 
+        scaledSize: new google.maps.Size(30, 45),
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(0, 0)
+    };
+    
+    var marker = new google.maps.Marker({
+        position: {lat: -37.816992, lng: 144.955793},
+        map: map,
+        icon: marker
+    });
 }
 
